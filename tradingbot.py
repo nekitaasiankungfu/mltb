eb91a3d0a9ad3a1c615fc039c268d563c491bffa
Nikita Tumanov <nikitatumanov2004@gmail.com>
2024-06-10 09:00:23 +0500

    first commit

--- tradingbot.py
+++ tradingbot.py
@@ -4,11 +4,11 @@ from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime 
 from alpaca_trade_api import REST 
-from timedelta import Timedelta 
+from timedelta import Timedelta
 from finbert_utils import estimate_sentiment
 
-API_KEY = "YOUR API KEY" 
-API_SECRET = "YOUR API SECRET" 
+API_KEY = "PKDPY7BJDV6IWTRO4GWU" 
+API_SECRET = "adeuy5tsgpZgyR1fBteMHT9H7pRuRPvaQnnAU7Jl" 
 BASE_URL = "https://paper-api.alpaca.markets"
 
 ALPACA_CREDS = {
